d919c5930fd3924ae2065f53c166225f45d79d2f
Aryan Blaauw <ablaauw@gmx.net>
2026-06-06 08:14:43 +0000

    Modify llama-server command in launch_nomid.sh
    Updated model path and added new parameters for llama-server.

--- tc/launch_nomid.sh
+++ tc/launch_nomid.sh
@@ -1,1 +1,1 @@
-./llama.cpp-embedded/build/bin/llama-server -m /media/aryan/nvme/models//nomic-embed-text-v2-moe.Q6_K.gguf --device VULKAN0 --embedding --host 0.0.0.0 --port 8034 --ctx-size 8192 --batch-size 1024 --n-gpu-layers 99 -np 1
+./llama.cpp-embedded/build/bin/llama-server -m /media/aryan/nvme/models/nomic-embed-text-v1.5.Q6_K.gguf --device VULKAN0 --embedding --host 0.0.0.0 --port 8034 --ctx-size 8192   -b 8192   -ub 8192  --batch-size 1024  --n-gpu-layers 99 -np 1
