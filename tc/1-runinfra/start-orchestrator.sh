88db6adc933a456a83f3f41cbdf29e7d14eff5f8
Aryan Blaauw <ablaauw@gmx.net>
2026-06-08 10:30:40 +0000

    Update memory allocation in start-orchestrator.sh
    Increased the memory allocation from 32768 to 40960.

--- tc/1-runinfra/start-orchestrator.sh
+++ tc/1-runinfra/start-orchestrator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/media/aryan/nvme/llama.cpp/llama.orch/build/bin/llama-server   -m /media/aryan/nvme/models/nvidia_Orchestrator-8B-Q6_K.gguf   -ngl 99   -c 32768   -b 512   -ub 512   --parallel 1   --no-mmap   --tools all   --jinja   --kv-unified   -fa on   -ctk q8_0   -ctv q4_0   -fit off   --host 0.0.0.0   --port 8080 --device VULKAN1
+/media/aryan/nvme/llama.orch/build/bin/llama-server   -m /media/aryan/nvme/models/nvidia_Orchestrator-8B-Q6_K.gguf   -ngl 99   -c 40960   -b 512   -ub 512   --parallel 1   --no-mmap   --tools all   --jinja   --kv-unified   -fa on   -ctk q8_0   -ctv q4_0   -fit off   --host 0.0.0.0   --port 8080 --device VULKAN1
