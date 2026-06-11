e5141017e3316036c6481bf1abdb3e393136db76
Aryan Blaauw <ablaauw@gmx.net>
2026-06-11 12:29:46 +0000

    move to autoresearch

--- tc/1-runinfra/launch_nomid.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/media/aryan/nvme/llama.cpp/llama.cpp-embedded/build/bin/llama-server -m /media/aryan/nvme/models/nomic-embed-text-v1.5.Q6_K.gguf --device VULKAN0 --embedding --host 0.0.0.0 --port 8034 --ctx-size 8192   -b 8192   -ub 8192  --batch-size 1024  --n-gpu-layers 99 -np 1
--- tc/1-runinfra/start-orchestrator.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/media/aryan/nvme/llama.orch/build/bin/llama-server   -m /media/aryan/nvme/models/nvidia_Orchestrator-8B-Q6_K.gguf   -ngl 99  --no-cache-idle-slots --cache-ram 0 -c 40960   -b 512   -ub 512   --parallel 2   --no-mmap   --tools all   --jinja   --kv-unified   -fa on   -ctk q8_0   -ctv q4_0   -fit off   --host 0.0.0.0   --port 8080 --device VULKAN1
